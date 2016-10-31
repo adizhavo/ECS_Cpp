07a4529108dd96e1e3228bb8f2a0d87cbec8e1fc
Adi Zhavo <adi@gram.gs>
2016-10-31 20:53:32 +0200

    Add first function implementation in Entity.cpp

--- ECS/Entity.cpp
+++ ECS/Entity.cpp
@@ -9,7 +9,33 @@ namespace ECS {
         // Remove All component
     }
     
-    std::list<Component> Entity::Components() {
-        return components;
+    Entity Entity::AddComponent(ECS::Component component, bool notifySystems) {
+        
+        return *this;
+    }
+    
+    Entity Entity::ReplaceComponent(ECS::Component component, bool notifySystems) {
+        
+        return *this;
+    }
+    
+    template<typename T> void Entity::RemoveComponent(T type) {
+        
+    }
+    
+    template<typename T> T Entity::GetComponent(T type) {
+        
+    }
+    
+    template<typename T> std::list<T> Entity::GetComponents(T type) {
+        
+    }
+    
+    template<typename T> bool HasComponent (T type) {
+        
+    }
+    
+    std::list<Component> Entity::GetComponents() {
+        return this->components;
     }
 }
