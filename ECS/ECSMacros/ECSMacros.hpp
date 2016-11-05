4c70552f7fe56c3fdb95b27ad8757624d03214e9
Adi Zhavo <adi@gram.gs>
2016-11-05 17:25:43 +0200

    Add macro i the example component for generating id

---
+++ ECS/ECSMacros/ECSMacros.hpp
@@ -0,0 +1,14 @@
+//
+//  ECSMacros.hpp
+//  ECS
+//
+//  Created by Adi Zhavo on 05/11/16.
+//  Copyright © 2016 Adi Zhavo. All rights reserved.
+//
+
+#ifndef ECSMacros_hpp
+#define ECSMacros_hpp
+
+#include <stdio.h>
+
+#endif /* ECSMacros_hpp */
