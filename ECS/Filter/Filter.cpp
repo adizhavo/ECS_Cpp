68f9df791244bced49fd37fd2c1c4cfe0016d6f2
Adi Zhavo <adi@gram.gs>
2016-11-05 17:42:08 +0200

    Small improvements

--- ECS/Filter/Filter.cpp
+++ ECS/Filter/Filter.cpp
@@ -28,14 +28,14 @@ namespace ECS {
     }
     
     void Filter::Reset() {
-        this->allOf.clear();
-        this->anyOf.clear();
-        this->noneOf.clear();
+        allOf.clear();
+        anyOf.clear();
+        noneOf.clear();
     }
     
     bool Filter::HasAnyMatcher(Entity* entity) {
         VECTOR_FOR_EACH(i, anyOf)
-        if (entity->HasComponent(this->anyOf.at(i)))
+        if (entity->HasComponent(anyOf.at(i)))
             return true;
         return this->anyOf.size() == 0;
     }
@@ -43,15 +43,15 @@ namespace ECS {
     bool Filter::HasAllMatchers(Entity* entity) {
         int componentCounter = 0;
         VECTOR_FOR_EACH(i, allOf)
-        if (entity->HasComponent(this->allOf.at(i)))
+        if (entity->HasComponent(allOf.at(i)))
             componentCounter ++;
         
-        return this->allOf.size() == 0 || (componentCounter != 0 && componentCounter == this->allOf.size());
+        return this->allOf.size() == 0 || (componentCounter != 0 && componentCounter == allOf.size());
     }
     
     bool Filter::HasNoneMatcher(Entity* entity) {
         VECTOR_FOR_EACH(i, noneOf)
-        if (entity->HasComponent(this->noneOf.at(i)))
+        if (entity->HasComponent(noneOf.at(i)))
             return false;
         return true;
     }
