708f7102b9dca06ee23bdc054ba4bfb3417038dc
Adi Zhavo <adi@gram.gs>
2016-11-06 12:13:16 +0200

    Update entity matcher

--- ECS/EntityMatcher/EntityMatcher.cpp
+++ ECS/EntityMatcher/EntityMatcher.cpp
@@ -4,18 +4,13 @@
 namespace ECS {
     std::vector<Entity*>EntityMatcher::subscribedEntities;
     
-    EntityMatcher::~EntityMatcher() {
-        subscribedEntities.clear();
-    }
-    
     void EntityMatcher::Subscribe(ECS::Entity* entity) {
         subscribedEntities.push_back(entity);
     }
     
     void EntityMatcher::UnSubscribe(ECS::Entity* entity) {
-        VECTOR_FOR_EACH(index, subscribedEntities) {
-            if (subscribedEntities[index] == entity)
+        VECTOR_FOR_EACH(index, subscribedEntities)
+            if (subscribedEntities[index] != NULL && subscribedEntities[index] == entity)
                 VECTOR_REMOVE(entity, subscribedEntities);
-        }
     }
 }
