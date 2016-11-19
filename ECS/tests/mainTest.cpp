1e82de7064f40979ce35e2ef58aa94432b0ba12f
Adi Zhavo <adi@gram.gs>
2016-11-19 19:03:24 +0200

    Refactor main, supports command line arguments

--- ECS/tests/mainTest.cpp
+++ ECS/tests/mainTest.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "filterTest.cpp"
+#include "entityTest.cpp"
 
-int main () {
-    return UnitTest::FilterTest::runTest();
+using namespace UnitTest;
+
+void execute(std::string test)
+{
+    if (test.compare("entity") == 0) EntityTest::runTest();
+    else if (test.compare("filter") == 0) FilterTest::runTest();
 }
+
+int main (int argc, char *argv[]) {
+    for (int i = 0; i < argc; i ++)
+        execute(argv[i]);
+    return 0;
+}
+
+
