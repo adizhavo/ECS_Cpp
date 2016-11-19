744e233235bd16960650d3a042c7ce873b49f716
Adi Zhavo <adi@gram.gs>
2016-11-19 18:48:01 +0200

    Add filter test

--- ECS/tests/filterTest.cpp
+++ ECS/tests/filterTest.cpp
@@ -1,9 +1,63 @@
-//
-//  filterTest.cpp
-//  ECS
-//
-//  Created by Adi Zhavo on 19/11/16.
-//  Copyright © 2016 Adi Zhavo. All rights reserved.
-//
+#include "test.hpp"
+#include "entity.hpp"
+#include "filter.hpp"
 
-#include <stdio.h>
+#define TEARDOWN                        \
+testEntity.RemoveAllComponents();       \
+filter.Reset();                         \
+
+
+namespace UnitTest {
+START_TEST(FilterTest)
+    Entity testEntity;
+    FirstTestComponent first;
+    SecondTestComponent second;
+    Filter filter;
+    
+    testEntity.AddComponent(&first);
+    filter.AnyOf(1, COMP_ID(FirstTestComponent));
+    ASSERT_TRUE(filter.DoesMatch(&testEntity), "Should match one (AnyOf) to entity with 1 component")
+    TEARDOWN
+    
+    testEntity.AddComponent(&first);
+    testEntity.AddComponent(&second);
+    filter.AnyOf(1, COMP_ID(SecondTestComponent));
+    ASSERT_TRUE(filter.DoesMatch(&testEntity), "Should match one (AnyOf) to entity with 2 components")
+    TEARDOWN
+    
+    testEntity.AddComponent(&first);
+    filter.AllOf(1, COMP_ID(FirstTestComponent));
+    ASSERT_TRUE(filter.DoesMatch(&testEntity), "Should match one (AllOf) to entity with 1 component")
+    TEARDOWN
+    
+    testEntity.AddComponent(&first);
+    filter.AllOf(2, COMP_ID(FirstTestComponent), COMP_ID(SecondTestComponent));
+    ASSERT_TRUE(!filter.DoesMatch(&testEntity), "Should not match two (AllOf) to entity with 1 component")
+    TEARDOWN
+    
+    testEntity.AddComponent(&first);
+    testEntity.AddComponent(&second);
+    filter.AllOf(2, COMP_ID(FirstTestComponent), COMP_ID(SecondTestComponent));
+    ASSERT_TRUE(filter.DoesMatch(&testEntity), "Should match two (AllOf) to entity with 2 components")
+    TEARDOWN
+    
+    testEntity.AddComponent(&second);
+    filter.NoneOf(1, COMP_ID(FirstTestComponent));
+    ASSERT_TRUE(filter.DoesMatch(&testEntity), "Should match one (NoneOf) to entity with 1 component")
+    TEARDOWN
+    
+    testEntity.AddComponent(&first);
+    filter.NoneOf(1, COMP_ID(FirstTestComponent));
+    ASSERT_TRUE(!filter.DoesMatch(&testEntity), "Should not match one (NoneOf) to entity with 1 component")
+    TEARDOWN
+    
+    testEntity.AddComponent(&first);
+    testEntity.AddComponent(&second);
+    filter.NoneOf(1, COMP_ID(FirstTestComponent));
+    ASSERT_TRUE(!filter.DoesMatch(&testEntity), "Should not match one (NoneOf) to entity with 2 components")
+    TEARDOWN
+    
+    TEARDOWN
+    
+END_TEST(0)
+}
